0728fa8972cdbb0979c9fd2b2772d99f2fd75110
KreskoLab <redeezol@gmail.com>
2022-08-22 18:22:22 +0300

    chore (strapi): enable proxy

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('ADMIN_URL', 'http://localhost:1337'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
