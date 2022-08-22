b20a41ea9fcfbaa0d183ea600b910ec3c27b2ab9
KreskoLab <redeezol@gmail.com>
2022-08-22 03:17:28 +0300

    chore (nuxt): miscs

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,13 +1,23 @@
 import { defineNuxtConfig } from 'nuxt'
-import eslintPlugin from 'vite-plugin-eslint'
+import UnpluginComponentsVite from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ['nuxt-windicss', '@nuxtjs/strapi'],
+	modules: ['nuxt-windicss', '@nuxtjs/strapi', 'unplugin-icons/nuxt', '@vueuse/nuxt'],
 
 	css: ['@/assets/main.css'],
 
 	vite: {
-		plugins: [eslintPlugin()],
+		plugins: [
+			UnpluginComponentsVite({
+				dts: false,
+				resolvers: [
+					IconsResolver({
+						prefix: 'Icon',
+					}),
+				],
+			}),
+		],
 	},
 })
