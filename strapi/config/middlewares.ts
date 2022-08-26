1e83554d17380720c54420f0bb278bf89d6767fc
KreskoLab <redeezol@gmail.com>
2022-08-26 17:43:15 +0300

    feat (strapi): update middleware

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,6 +1,5 @@
 export default [
   'strapi::errors',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -8,6 +7,14 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      origin: process.env.MERCHANT_RETURN_URL
+    }
+  },
   {
     name: 'strapi::security',
     config: {
